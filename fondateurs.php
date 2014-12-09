447553bba0b34e20553f22cc6b1f1f2b23edec6d
Willems1994 <henrywillems@hotmail.fr>
2014-12-09 16:06:47 +0100

    Create fondateurs.php

---
+++ fondateurs.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta name="description" content="" />
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Home Switch Home</title>
+		<link href="http://fonts.googleapis.com/css?family=Oxygen:400,700,300" rel="stylesheet" type="text/css" />
+		<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+	</head>
+
+	<body>
+		<div id="wrapper">
+			<?php include("header.php") ?>
+			<div id="logo" class="container">
+
+		
+				<h1>Home Switch Home</h1>
+				<p>Le site d'échange de logement entre particuliers</p>
+	
+			</div>
+			<div id="fondateurs">
+				<p> Nos Fondateurs :<br /> 
+					<ul>
+						<li>Kevin Westerdyk<br /><br /><img src="Kevin.jpg" alt="Kevin Westerdyk" id="img" /></li>
+						<li>Danny Canaan<br /><br /><img src="Danny.jpg" alt="Danny Canaan" id="img" /></li>
+						<li>Hugo Georget<br /><br /><img src="Hugo.jpg" alt="Hugo Georget" id="img" /></li>
+						<li>Jordan Menache<br /><br /><img src="Jordan.jpg" alt="Jordan Menache" id="img" /></li>
+						<li>Henry Willems<br /><br /><img src="Henry.jpg" alt="Henry Willems" id="img" /></li>
+						<li>Paul Verneuil</li>
+					</ul>
+				</p>
+			</div>	
+		</div>
+
+			<div id="footer">
+		
+			<?php include("footer.php"); ?>
+	
+		</div>
+	
+	</body>
+
+</html>
