63350ab8a2b5b72d58f433e40fc486abd9b61b80
HugoGEORGET <hugogeorget1994@gmail.com>
2014-12-09 15:03:36 +0100

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Home Switch Home</title>
+<link href="http://fonts.googleapis.com/css?family=Oxygen:400,700,300" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div id="wrapper">
+	
+	<?php include("header.php") ?>
+
+	<div id="logo" class="container">
+		<h1><a href="#">Home Switch Home</a></h1>
+		<p>Le site d'échange de logement entre particuliers</p>
+	</div>
+	<div id = "page" class="container">
+		<h1>Pour nous contacter par mail :</h1></br>
+		<ul>
+			<li>CANAAN Danny : <a href="mailto:danny.canaan@isep.fr">danny.canaan@isep.fr</a></li>
+			<li>GEORGET Hugo : <a href="mailto:hugo.georget@isep.fr">hugo.georget@isep.fr</a></li>
+			<li>MENACHE Jordan : <a href="mailto:jordan.menache@isep.fr">jordan.menache@isep.fr</a></li>
+			<li>VERNEUIL Paul : <a href="mailto:paul.verneuil@isep.fr">paul.verneuil@isep.fr</a></li>
+			<li>WESTERDYK Kévin : <a href="mailto:kevin.westerdyk@isep.fr">kevin.westerdyk@isep.fr</a></li>
+			<li>WILLEMS Henry : <a href="mailto:henry.willems@isep.fr">henry.willems@isep.fr</a></li>
+		</ul>
+		<p>Vous pouvez aussi nous contacter par le biais de la messagerie privée.</p>
+	</div>
+</div>
+<div id="footer">
+	<p>Flemme de taper le footer...</p>
+</div>
+<!-- end #footer -->
+</body>
+</html>
