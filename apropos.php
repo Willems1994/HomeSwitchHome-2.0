b146934718ddfc4e042376cc05b25e1c5ee11209
Willems1994 <henrywillems@hotmail.fr>
2014-12-09 16:06:03 +0100

    Create apropos.php

---
+++ apropos.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta name="description" content="" />
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Home Switch Home</title>
+		<link href="http://fonts.googleapis.com/css?family=Oxygen:400,700,300" rel="stylesheet" type="text/css" />
+		<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+	</head>
+
+	<body>
+		<div id="wrapper">
+			<?php include("header.php") ?>
+			<div id="logo" class="container">
+
+		
+				<h1>Home Switch Home</h1>
+				<p>Le site d'échange de logement entre particuliers</p>
+	
+			</div>
+			
+	
+			<div id="leftnav">
+				
+				<ul>
+					<li><a href="apropos.php">A propos</a></li>
+					<li><a href="fondateurs.php">Fondateurs</a></li>
+					
+				</ul>
+
+			</div>
+			<div id="apropos">
+				<h1>A Propos de nous :</h1>
+				<br />
+				<br />
+				<p> Créé en 2014, dans le cadre d'un projet d'étude à l'ISEP, l'APP, le site Home Switch Home et un site qui vous permet d'échanger vos logements en toute confiance et vous permet de bénéficier de vacances à moindre coûts, en toute sérénité. 
+				</p>
+				<br />
+				<p>Qu'il s'agisse de vacances en France ou à l'étranger, seul ou à plusieurs, Home Switch Home vous propose des vacances de rêves à la destination de votre choix. Mer, montagne ou campagne ? Seul, en famille ou en entre amis ? N'hésitez plus et rejoignez la communauté Home Switch Home pour des séjours inoubliables.</p>
+				<p>Vous n'êtes pas encore inscrit ? Inscrivez vous <a href="inscription.php">ici</a></p>
+				<p>Déjà membre ? Venez découvrir nos nouveaux logements disponibles <a href="logement.php">ici</a></p>
+			</div>
+
+		
+		</div>
+		<div id="footer">
+		
+			<?php include("footer.php"); ?>
+	
+		</div>
+	
+	</body>
+
+</html>
